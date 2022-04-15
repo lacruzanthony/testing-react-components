9f5940748a86d9203d6e8f0af492021d2282dbf9
lacruzanthony <lacruzanthony@users.noreply.github.com>
2022-04-14 22:25:00 -0500

    Dark mode Dark mode added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./styles.css";
 
 export default function App() {
+  const handlerTheme = () => {
+    document.body.classList.toggle("dark-theme");
+  };
+
   return (
-    <div className="App">
+    <div className="App dark-theme || light-theme">
       <h1>Hello CodeSandbox!</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button className="btn-toggle" onClick={handlerTheme}>
+        Change theme
+      </button>
     </div>
   );
 }
