3042bc6ca55a031be6710af30be4300f84cc766b
lacruzanthony <lacruzanthony@users.noreply.github.com>
2022-04-15 17:08:09 -0500

    test checking clicks

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,11 +9,15 @@ describe("empty", () => {
 
 describe("App theme", () => {
   it("should toggle the theme", () => {
+    const mockHandler = jest.fn();
     const { getByTestId } = render(<App />);
     const toggleBtn = getByTestId("theme-button");
+    toggleBtn.onclick = mockHandler;
     fireEvent.click(toggleBtn);
+    expect(mockHandler).toHaveBeenCalledTimes(1);
     expect(getByTestId("app-test")).toHaveClass("dark-theme");
     fireEvent.click(toggleBtn);
+    expect(mockHandler).toHaveBeenCalledTimes(2);
     expect(getByTestId("app-test")).toHaveClass("light-theme");
   });
 });
