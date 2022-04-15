3a63c110889f3e7ab0dadd70796ce57b9ac0ca3b
lacruzanthony <lacruzanthony@users.noreply.github.com>
2022-04-15 14:38:56 -0500

    test added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,19 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
+
 describe("empty", () => {
   it("should work", () => {
     expect(true).toEqual(true);
+  });
+});
+
+describe("App theme", () => {
+  it("should toggle the theme", () => {
+    const { container } = render(<App />);
+    console.log(container.classList);
+    fireEvent.click(screen.getByText("Change theme"));
+    console.log(container.classList);
+
+    // expect(screen.getByText("Hello CodeSandbx!").cla.color).toBe("#eee");
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   };
 
   return (
-    <div className="App dark-theme || light-theme">
+    <div data-testid="test-container" className="App dark-theme || light-theme">
       <h1>Hello CodeSandbox!</h1>
       <h2>Start editing to see some magic happen!</h2>
       <button className="btn-toggle" onClick={handlerTheme}>
